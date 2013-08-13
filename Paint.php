5c5bbd5b05380be81c70c8127eee62fcfaecf9ec
Quentin AUPETIT <qaupetit@MacBook-Pro-de-Quentin-AUPETIT.local>
2013-08-13 20:42:30 +0200

    adding test files

--- Paint.php
+++ Paint.php
@@ -6,6 +6,14 @@ use Paint\Filter\FilterInterface;
 
 class Paint
 {
+	public static $AVAILABLE_FORMATS = array(
+		IMAGETYPE_JPEG,
+		IMAGETYPE_PNG,
+		IMAGETYPE_GIF,
+		IMAGETYPE_WBMP,
+		IMAGETYPE_XBM
+	);
+
 	public $inputPath;
 	public $outputPath;
 
@@ -16,10 +24,23 @@ class Paint
 	protected $inputHeight;
 	protected $inputType;
 
+	protected $outputFormat = IMAGETYPE_JPEG;
+
+	protected $outputWidth = 0;
+	protected $outputHeight = 0;
+
+	protected $outputQuality = 100;
+
+	protected $foreground = array(0, 0, 0);
+
+	protected $pngFilters;
+
+
 	public function __construct()
 	{
 
 	}
+
 
 	public function __destruct()
 	{
@@ -31,15 +52,35 @@ class Paint
 		}
 	}
 
+
+	/**
+	 * undocumented class variable
+	 *
+	 * @param string octet du format d'image. Formats supportés : IMG_GIF | IMG_JPG | IMG_PNG | IMG_WBMP | IMG_XPM
+	 **/
+	public static function isSupportedFormats($format)
+	{
+		return true == (imagetypes() & $format);
+	}
+
+
+	public static function validColor($color)
+	{
+		return min(abs((int) $color), 255);
+	}
+
+
 	public static function create()
 	{
 		return new static();
 	}
 
+
 	public function input($input)
 	{
-		if (!is_string($input) && !is_file($input)) {
-			throw new \InvalidArgumentException("Input file is not a valid ressource");
+		// if is not a string, not an existing file or not a file ressource
+		if (!is_string($input) || !file_exists($input) || !is_file($input)) {
+			throw new \InvalidArgumentException('Input file is not a valid ressource.');
 		}
 
 		list($this->inputWidth, $this->inputHeight, $this->inputType) = getimagesize($input);
@@ -60,6 +101,9 @@ class Paint
 			case IMAGETYPE_XBM:
 				$this->input = imagecreatefromxbm($input);
 				break;
+			case IMAGETYPE_XPM:
+				$this->input = imagecreatefromxpm($input);
+				break;
 			default:
 				throw new \InvalidArgumentException('Unsuported file type.');
 				break;
@@ -68,25 +112,99 @@ class Paint
 		$this->inputPath = $input;
 	}
 
+
 	public function output($output)
 	{
 		if (!is_string($output)) {
-			throw new \InvalidArgumentException("Output file is not a valid ressource");
+			throw new \InvalidArgumentException('Output file is not a valid ressource.');
 		}
 
 		if (is_file($output)) {
-			trigger_error("Output file already exists", E_USER_NOTICE);
+			trigger_error('Output file already exists.', E_USER_NOTICE);
 		}
 
 		if (!is_writable(dirname($output))) {
-			throw new \InvalidArgumentException("Output file is not writable");
+			throw new \InvalidArgumentException('Output file is not writable.');
 		}
 
 		$this->outputPath = $output;
 	}
 
+
 	public function outputFormat($format)
 	{
+		if (false === self::isSupportedFormats($format)) {
+			throw new \InvalidArgumentException('This output format is not supported.');
+		}
 
+		$this->outputFormat = $format;
+	}
+
+
+	public function setOutputWidth($width)
+	{
+		$this->outputWidth = abs((int) $width);
+	}
+
+
+	public function setOutputHeight($height)
+	{
+		$this->outputHeight = abs((int) $height);
+	}
+
+
+	public function setOutputSize($width, $height)
+	{
+		$this->setOutputWidth($width);
+		$this->setOutputHeight($height);
+	}
+
+
+	public function setQuality($quality = 100)
+	{
+		$this->outputQuality = abs((int) $quality);
+	}
+
+
+	public function setForeground($red, $green, $blue)
+	{
+		$this->foreground = array(
+			self::validColor($red),
+			self::validColor($green),
+			self::validColor($blue)
+		);
+	}
+
+
+	public function generate()
+	{
+		if (0 >= $this->outputWidth || 0 >= $this->outputHeight) {
+			throw new \LengthException('Invalid image dimensions');
+		}
+
+		$this->output = imagecreatetruecolor($this->outputWidth, $this->outputHeight);
+
+		switch ($this->outputFormat) {
+			case IMG_JPEG:
+				imagejpeg($this->output, $this->outputPath, $this->outputQuality);
+				break;
+			case IMG_PNG:
+				imagepng($this->output, $this->outputPath, $this->outputQuality, $this->pngFilters);
+				break;
+			case IMG_GIF:
+				imagegif($this->output, $this->outputPath);
+				break;
+			case IMG_WBMP:
+				$foreground = imagecolorallocate($this->output, $this->foreground[0], $this->foreground[1], $this->foreground[2]);
+				imagewbmp($this->output, $this->outputPath, $foreground);
+				break;
+			case IMG_XPM:
+				$foreground = imagecolorallocate($this->output, $this->foreground[0], $this->foreground[1], $this->foreground[2]);
+				imagexbm($this->output, $this->outputPath, $foreground);
+				break;
+			default:
+				throw new \InvalidArgumentException('Unknow output format.');
+				break;
+		}
 	}
 }
--- Tests/PaintTest.php
+++ Tests/PaintTest.php
@@ -44,6 +44,7 @@ class PaintTest extends \PHPUnit_Framework_TestCase
 	public function testOutputExists()
 	{
 		$this->setExpectedException('PHPUnit_Framework_Error_Notice');
+
 		$paint = Paint::create();
 		$paint->output('Tests/carlos.jpeg');
 	}
@@ -51,7 +52,78 @@ class PaintTest extends \PHPUnit_Framework_TestCase
 	public function testOutputNotWritable()
 	{
 		$this->setExpectedException('InvalidArgumentException');
+
 		$paint = Paint::create();
 		$paint->output('Tests/undefined/carlos.jpeg');
 	}
+
+	public function testSupportedFormat()
+	{
+		$this->assertEquals(true, Paint::isSupportedFormats(IMG_GIF));
+		$this->assertEquals(true, Paint::isSupportedFormats(IMG_JPG));
+		$this->assertEquals(true, Paint::isSupportedFormats(IMG_PNG));
+		$this->assertEquals(true, Paint::isSupportedFormats(IMG_WBMP));
+	}
+
+	public function testUnsupportedFormat()
+	{
+		$paint = Paint::create();
+		$this->assertEquals(false, Paint::isSupportedFormats('unknow'));
+	}
+
+	public function testValidOutputFormat()
+	{
+		$paint = Paint::create();
+		$paint->outputFormat(IMG_GIF);
+		$paint->outputFormat(IMG_JPEG);
+		$paint->outputFormat(IMG_PNG);
+		$paint->outputFormat(IMG_WBMP);
+	}
+
+	public function testInvalidOutputFormat()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+
+		$paint = Paint::create();
+		$paint->outputFormat('unknow');
+	}
+
+	public function testGenerate()
+	{
+		if (file_exists('Tests/output.jpeg')) {
+			unlink('Tests/output.jpeg');
+		}
+
+		$paint = Paint::create();
+		$paint->setOutputSize(100, 100);
+		$paint->output('Tests/output.jpeg');
+		$paint->generate();
+
+		$this->assertEquals(true, file_exists('Tests/output.jpeg'));
+	}
+
+	// public function testWbmpForeground()
+	// {
+	// 	$file = 'Tests/output.wbmp';
+
+	// 	if (file_exists($file)) {
+	// 		unlink($file);
+	// 	}
+
+	// 	$paint = Paint::create();
+	// 	$paint->setOutputSize(100, 100);
+	// 	$paint->setForeground(255, 0, 0);
+	// 	$paint->outputFormat(IMG_WBMP);
+	// 	$paint->output($file);
+	// 	$paint->generate();
+
+	// 	$this->assertEquals(true, file_exists($file));
+
+	// 	$img = imagecreatefromwbmp($file);
+	// 	$rgb = imagecolorsforindex($img, imagecolorat($img, 1, 1));
+
+	// 	$this->assertEquals(255, $rgb['red']);
+	// 	$this->assertEquals(0, $rgb['green']);
+	// 	$this->assertEquals(0, $rgb['blue']);
+	// }
 }
