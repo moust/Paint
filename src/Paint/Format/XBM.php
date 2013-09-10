0cdd3e36f7e92599c691f606f7c9257bfb784165
Quentin Aupetit <quentin@cellules.tv>
2013-09-10 16:03:16 +0200

    add XBM output format

---
+++ src/Paint/Format/XBM.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Paint\Format;
+
+use Paint\Color;
+
+class XBM implements FormatInterface
+{
+	public $foreground;
+
+	/**
+	 * Constructor
+	 *
+	 * @param int $red JPEG Compression level: from 0 to 100 (no compression).
+	 **/
+	public function __construct(Color $foreground = null)
+	{
+		if (!is_null($foreground)) {
+			$this->foreground = $foreground;
+		}
+	}
+
+	public function generate($output, $outputPath = null)
+	{
+		// TODO : WBMP foreground seem doesn't work...
+		if (!is_null($this->foreground)) {
+			imagexbm($output, $outputPath, $this->foreground->getColor());
+		}
+		else {
+			imagexbm($output, $outputPath);
+		}
+	}
+}
